d7fa0db818da539226a11f798fa80d0440bb773d
icextreme <74882450+icextreme@users.noreply.github.com>
2021-02-20 18:52:02 -0800

    Add buttons to move between tips

--- app/src/main/java/com/example/stormhacks2021/tips/TipsActivity.java
+++ app/src/main/java/com/example/stormhacks2021/tips/TipsActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.text.method.LinkMovementMethod;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,14 +16,14 @@ import com.example.stormhacks2021.MapActivity;
 import com.example.stormhacks2021.MessageActivity;
 import com.example.stormhacks2021.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
 public class TipsActivity extends AppCompatActivity {
-
+    int index;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,10 +33,36 @@ public class TipsActivity extends AppCompatActivity {
         setNavigationView();
         List<String> tips = Arrays.asList(getResources().getStringArray(R.array.tips));
 
-        int index = new Random().nextInt(tips.size());
+        index = new Random().nextInt(tips.size());
+
+        Log.d("index", String.valueOf(index));
 
         TextView tipsTextView = findViewById(R.id.tip_TextView);
         tipsTextView.setText(tips.get(index));
+        tipsTextView.setMovementMethod(LinkMovementMethod.getInstance());
+
+        FloatingActionButton backBtn = findViewById(R.id.fab_tips_back);
+        FloatingActionButton fwdBtn = findViewById(R.id.fab_tips_forward);
+
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (index > 0) {
+                    index--;
+                }
+                tipsTextView.setText(tips.get(index));
+            }
+        });
+
+        fwdBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (index < tips.size() - 1) {
+                    index++;
+                }
+                tipsTextView.setText(tips.get(index));
+            }
+        });
     }
 
     @Override
