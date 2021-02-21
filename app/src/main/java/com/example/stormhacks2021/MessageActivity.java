203a95b820535f2775ae3ba36bedc3ec03466bc8
InderdeepM <inderdeepmanan@gmail.com>
2021-02-20 16:58:01 -0800

    Creates new add user screen

--- app/src/main/java/com/example/stormhacks2021/MessageActivity.java
+++ app/src/main/java/com/example/stormhacks2021/MessageActivity.java
@@ -12,6 +12,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MessageActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/example/stormhacks2021/NewUserActivity.java
+++ app/src/main/java/com/example/stormhacks2021/NewUserActivity.java
@@ -2,13 +2,54 @@ package com.example.stormhacks2021;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class NewUserActivity extends AppCompatActivity {
+    private EditText mName, mEmail, mInterests;
+    private Button submitBtn;
 
+    private FirebaseDatabase db = FirebaseDatabase.getInstance();
+    private DatabaseReference root = db.getReference().child("Users");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_user);
+
+        mName = findViewById(R.id.nameEditText);
+        mEmail = findViewById(R.id.emailEditText);
+        mInterests = findViewById(R.id.interestsEditText);
+        submitBtn = findViewById(R.id.submitBtn);
+
+        submitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = mName.getText().toString();
+                String email = mEmail.getText().toString();
+                String interests = mInterests.getText().toString();
+
+                HashMap<String , String> userMap = new HashMap<>();
+                userMap.put("name", name);
+                userMap.put("email", email);
+                userMap.put("interests", interests);
+
+                root.push().setValue(userMap);
+
+                Context context = getApplicationContext();
+                CharSequence text = "Submitted!";
+                int duration = Toast.LENGTH_SHORT;
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
     }
 }
